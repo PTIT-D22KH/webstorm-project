2394fa0702a3da56ac5c7b4bef268e2911bac4eb
duong vu cong tuan <vucongtuanduong2409@gmail.com>
2025-01-24 22:19:05 +0700

    video 31

--- test3/src/components/learn/MyComponent.jsx
+++ test3/src/components/learn/MyComponent.jsx
@@ -1,9 +1,22 @@
 import './style.css';
 
 const MyComponent = () => {
+    // const hoidanit = "eric 1"; //string
+    // const hoidanit = 25; //number
+    // const hoidanit = true; //boolean
+    // const hoidanit = undefined;
+    // const hoidanit = null;
+    // const hoidanit = [1,2,3];
+    const hoidanit = {
+        name: "hoidanit",
+        age: 25
+    }
+
+
     return (
         <>
-            <div>Hoi dan IT with Eric</div>
+            <div>Hoi dan IT with {JSON.stringify(hoidanit)}</div>
+            <div>{console.log("ERIC")}</div>
             <div className={"child"}
             style={
                 {borderRadius: "10px"}
