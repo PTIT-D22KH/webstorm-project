a2c0f67c9aa005e5df0dee659afef5912cf4d6d3
duong vu cong tuan <vucongtuanduong2409@gmail.com>
2025-01-25 14:05:34 +0700

    video 36

--- test5/test/src/components/todo/ToDoNew.jsx
+++ test5/test/src/components/todo/ToDoNew.jsx
@@ -1,10 +1,25 @@
 const ToDoNew = (props) => {
     const {addNewTodo} = props;
-    addNewTodo("Eric");
+    // addNewTodo("Eric");
+    const handleClick = () => {
+        alert("Click me!");
+    }
+    const handleOnChange = (name) => {
+        console.log(">>>>>>>> handleOnChange", name);
+    }
     return (
         <div className={"todo-new"}>
-            <input type={"text"} placeholder={"Enter your task"}/>
-            <button>Add</button>
+            <input
+                type={"text"}
+                placeholder={"Enter your task"}
+                onChange={(event) => handleOnChange(event.target.value)}
+            />
+            <button
+                style={{cursor: "pointer"}}
+                onClick={handleClick}
+            >
+                Add
+            </button>
         </div>
     )
 }
